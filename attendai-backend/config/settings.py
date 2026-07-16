ecbe62fc86ddf6fae1a45b8461b05314879c616c
Sahil Bhardwaj <smsbhracdwaj2006@gmail.com>
2026-07-17 03:07:00 +0530

    change

--- attendai-backend/config/settings.py
+++ attendai-backend/config/settings.py
@@ -76,14 +76,25 @@ ASGI_APPLICATION = 'config.asgi.application'
 
 # --- Database (PostgreSQL) ---
 import dj_database_url
+import os
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3',
-        conn_max_age=600,
-        ssl_require=True,
-    )
-}
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if DATABASE_URL:
+    DATABASES = {
+        "default": dj_database_url.parse(
+            DATABASE_URL,
+            conn_max_age=600,
+            ssl_require=True,
+        )
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 AUTH_USER_MODEL = 'accounts.User'
 
